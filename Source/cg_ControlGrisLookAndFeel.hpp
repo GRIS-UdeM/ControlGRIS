790636382031778100b72bb63d229374bcf95eb3
Vincent Berthiaume <vberthiaume@gmail.com>
2025-08-11 17:05:03 -0400

    trying to build [skip ci]

--- Source/cg_ControlGrisLookAndFeel.hpp
+++ Source/cg_ControlGrisLookAndFeel.hpp
@@ -25,18 +25,21 @@
 namespace gris
 {
 //==============================================================================
-class GrisLookAndFeel final : public juce::LookAndFeel_V3
+class GrisLookAndFeel final : public juce::LookAndFeel_V4
 {
     float mFontSize;
-    juce::Font mFont{ juce::CustomTypeface::createSystemTypefaceFor(
-        BinaryData::SinkinSans400Regular_otf,
-        static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
-    juce::Font mBigFont{ juce::CustomTypeface::createSystemTypefaceFor(
-        BinaryData::SinkinSans400Regular_otf,
-        static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
-    juce::Font mBiggerFont{ juce::CustomTypeface::createSystemTypefaceFor(
-        BinaryData::SinkinSans400Regular_otf,
-        static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
+    juce::Font mFont{ juce::FontOptions(BinaryData::SinkinSans400Regular_otf,
+                                        juce::Font::plain,
+                                        static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
+    juce::Font mNumSliderFont{ juce::FontOptions(BinaryData::SinkinSans400Regular_otf,
+                                                 juce::Font::plain,
+                                                 static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
+    juce::Font mBigFont{ juce::FontOptions(BinaryData::SinkinSans400Regular_otf,
+                                           juce::Font::plain,
+                                           static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
+    juce::Font mBiggerFont{ juce::FontOptions(BinaryData::SinkinSans400Regular_otf,
+                                              juce::Font::plain,
+                                              static_cast<size_t>(BinaryData::SinkinSans400Regular_otfSize)) };
 
     juce::Colour mBackgroundAndFieldColor;
     juce::Colour mWinBackgroundAndFieldColor;
